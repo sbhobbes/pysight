599e69246e15a81b986d1ac1512e6ee2fa4a318a
Hobbes <Seth.Hobbes@worthingtonindustries.com>
2023-09-13 11:29:56 -0400

    Some changes that I don't remember.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from pysight import fpp3
 
-fpp3.chapter_2.section_2_4.seasonal_plots.seasonal_plots()
+df = fpp3.chapter_5.section_5_2.some_simple_forecasting_methods.get_bricks_data()
+# mean_df = fpp3.chapter_5.section_5_2.some_simple_forecasting_methods.get_forecast(df, 'Bricks', 'Quarter_date', 'mEAN', 25, 'QS')
+fpp3.chapter_5.section_5_2.some_simple_forecasting_methods.plot_mean_ts(df, 25, 'mean_bricks', 'Quarter_date', 'QS')
+fc_df = fpp3.chapter_5.section_5_2.some_simple_forecasting_methods.get_forecast(df, 'Bricks', 'Quarter_date', 'Naive', 25, 'QS')
+fpp3.chapter_5.section_5_2.some_simple_forecasting_methods.plot_naive_ts(df, fc_df, 'Bricks', 'naive_bricks')
--- pysight/fpp3/chapter_5/section_5_2/some_simple_forecasting_methods.py
+++ pysight/fpp3/chapter_5/section_5_2/some_simple_forecasting_methods.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os 
 import statsmodels.api as sm
+import math
 from .... import helper
 
 def get_bricks_data():
@@ -16,53 +17,63 @@ def get_bricks_data():
 
     return bricks_df
 
+def get_forecast(df, value_col, ts_col, model, n_periods = 12, forecast_frequency = 'QS', seasonal_period = 4):
+    MEAN = 'mean'
+    NAIVE = 'naive'
 
-
-def some_simple_forecasting_methods():
-    aus_production_df = helper.load_data('pysight\\fpp3\\assets\\data\\chapter_5\\aus_production.csv')
-    aus_production_df['Quarter_date'] = aus_production_df['Quarter'].apply(helper.quarter_to_datetime)
-
-    aus_production_df.set_index('Quarter', inplace = True)
-    aus_production_df.drop(columns = ['Unnamed: 0'], inplace = True)
-    bricks_df = aus_production_df.loc['1970 Q1' : '2004 Q4']
-    bricks_df['mean_bricks'] = np.mean(bricks_df['Bricks'])
-
-    n_periods = 25
-    last_date = bricks_df['Quarter_date'].iloc[-1]
-    forecast_dates = pd.date_range(start = last_date, periods = n_periods, inclusive = 'right', freq = 'QS')
-    forecast_df = pd.DataFrame(forecast_dates, columns = ['Quarter_date'])
-    forecast_df['mean_bricks'] = np.min(bricks_df['mean_bricks'])
-    forecast_df['Quarter'] = forecast_df['Quarter_date'].apply(helper.datetime_to_quarter)
+    last_date = df[ts_col].iloc[-1]
+    forecast_dates = pd.date_range(start = last_date, periods = n_periods, inclusive = 'right', freq = forecast_frequency)
+    forecast_df = pd.DataFrame(forecast_dates, columns = [ts_col])
+    forecast_df[value_col] = np.min(df[value_col])
+    forecast_df['Quarter'] = forecast_df[ts_col].apply(helper.datetime_to_quarter)
     forecast_df.set_index('Quarter', inplace = True)
 
-    # Plot bricks with mean line
+    if str.lower(model) == MEAN:
+        pass
+    elif str.lower(model) == NAIVE:
+        forecast = df[value_col].tail(seasonal_period)
+        forecast_addend = forecast.copy()
+        repetitions = int(len(forecast_df) / seasonal_period)
+        for rep in range(repetitions - 1):
+            forecast = pd.concat([forecast, forecast_addend])
+        forecast_df['seasonal_naive_bricks'] = forecast.values
+
+    return forecast_df
+
+def plot_mean_ts(df, n_periods, value_col, ts_col, forecast_frequency):
+    mean_fc_df = get_forecast(df, value_col, ts_col, 'Mean', n_periods, forecast_frequency)
     plt.figure(figsize = (12, 7))
-    sns.lineplot(data = bricks_df, x = bricks_df.index, y = 'Bricks', color = 'black')
-    sns.lineplot(data = bricks_df, x = bricks_df.index, y = 'mean_bricks', color = 'blue', linestyle = '--')
-    sns.lineplot(data = forecast_df, x = forecast_df.index, y = 'mean_bricks', color = 'blue')
+    sns.lineplot(data = df, x = df.index, y = 'Bricks', color = 'black')
+    sns.lineplot(data = df, x = df.index, y = value_col, color = 'blue', linestyle = '--')
+    sns.lineplot(data = mean_fc_df, x = mean_fc_df.index, y = value_col, color = 'blue')
     plt.title('Mean Forecast')
     ticks = plt.xticks()[0]
     plt.xticks(ticks[::10])
     plt.tight_layout()
     plt.show()
 
-    # Naive method
+def plot_naive_ts(y_df, y_hat_df, y_col, y_hat_col):
     plt.figure(figsize = (12, 7))
-    forecast_df['naive_bricks'] = bricks_df['Bricks'].iloc[-1]
-    sns.lineplot(data = bricks_df, x = bricks_df.index, y = 'Bricks', color = 'black')
-    sns.lineplot(data = forecast_df, x = forecast_df.index, y = 'naive_bricks', color = 'blue')
+    y_hat_df[y_hat_col] = y_df[y_col].iloc[-1]
+    sns.lineplot(data = y_df, x = y_df.index, y = y_col, color = 'black')
+    sns.lineplot(data = y_hat_df, x = y_hat_df.index, y = y_hat_col, color = 'blue')
     plt.title('Naive Forecast')
+    ticks = plt.xticks()[0]
     plt.xticks(ticks[::10])
     plt.tight_layout()
     plt.show()
 
-    # Seasonal Naive method
-    def seasonal_naive_forecast(series, seasonal_period):
-        return series.tail(seasonal_period)
+def some_simple_forecasting_methods():
+    bricks_df = get_bricks_data()
+    plot_mean_ts(bricks_df, 25, 'mean_bricks', 'Quarter_date', 'QS')
 
-    forecast = seasonal_naive_forecast(bricks_df['Bricks'], 4)
-    forecast = pd.concat([forecast, forecast, forecast, forecast, forecast, forecast])
-    forecast_df['seasonal_naive_bricks'] = forecast.values
+    # Seasonal Naive method
+    # def seasonal_naive_forecast(series, seasonal_period):
+    #     return series.tail(seasonal_period)
+
+    # forecast = seasonal_naive_forecast(bricks_df['Bricks'], 4)
+    # forecast = pd.concat([forecast, forecast, forecast, forecast, forecast, forecast])
+    # forecast_df['seasonal_naive_bricks'] = forecast.values
 
     plt.figure(figsize = (12, 7))
     sns.lineplot(data = bricks_df, x = bricks_df.index, y = 'Bricks', color = 'black')
@@ -103,4 +114,3 @@ def some_simple_forecasting_methods():
 
     # Show 3 alongside actual data
     beer_df = aus_production_df.loc['1992 Q1' : '2006 Q4']
-
